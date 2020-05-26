a75598c6e0866a7ae7b47ebe7353a62f4a97e138
cjlog <1594684234@qq.com>
2020-05-26 23:48:15 +0800

    【新增】初次新建库

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Demo1 from "./component/demo1";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Demo1></Demo1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/demo1.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import './demo1.less';
+class Demo1 extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className={"demo1"}>
+                我是react demo1
+            </div>
+        );
+    }
+}
+
+export default Demo1;
